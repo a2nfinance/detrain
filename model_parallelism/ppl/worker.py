5635d801a1ba8d03bb43879704da621632824552
a2nfinance <levi@a2n.finance>
2024-04-27 23:01:32 +0700

    Add worker & master nodes

---
+++ model_parallelism/ppl/worker.py
@@ -0,0 +1,29 @@
+import torch.distributed.rpc as rpc
+from master_node import run_master
+def run_worker(rank, world_size, model, train_dataloader, test_dataloader, loss_fn, optimizer, epochs, batch_size):
+    # Higher timeout is added to accommodate for kernel compilation time in case of ROCm.
+    options = rpc.TensorPipeRpcBackendOptions(num_worker_threads=256, rpc_timeout=300)
+    
+    if rank == 0:
+        print("--- Init master RPC")
+        rpc.init_rpc(
+            "master",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        print("--- Done init master")
+        run_master(model, train_dataloader, test_dataloader, loss_fn, optimizer, epochs, batch_size)
+    else:
+        print(f"--- Init worker {rank} RPC")
+        rpc.init_rpc(
+            f"worker{rank}",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        print(f"--- Done Init worker {rank} RPC")
+        pass
+
+    # block until all rpcs finish
+    rpc.shutdown()
