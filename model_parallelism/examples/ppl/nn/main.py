5050bebf6b692acd7f750cab5a77f865ba359132
a2nfinance <levi@a2n.finance>
2024-04-28 17:45:59 +0700

    add model training example with pipeline

---
+++ model_parallelism/examples/ppl/nn/main.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+import time
+import sys
+from detrain.ppl.args_util import get_args
+from detrain.ppl.worker import run_worker
+from detrain.ppl.dis_model import DistributedModel
+from detrain.ppl.dataset_util import get_torchvision_dataset
+from shards_model import NNShard1, NNShard2
+from torch.distributed.optim.optimizer import DistributedOptimizer
+import torch.optim as optim
+# Define model here
+# torchrun \
+# --nproc_per_node=1 --nnodes=3 --node_rank=0 \
+# --master_addr=localhost --master_port=9999 \
+# main.py --use_syn
+if __name__=="__main__":
+    args = get_args()
+    print(args)
+    # Get args
+    world_size = int(args.world_size)
+    rank = int(args.rank)
+    epochs = int(args.epochs)
+    batch_size = int(args.batch_size)
+    device = "cpu"
+
+    # Check devices
+    if (args.gpu is not None):
+        device = "cuda:0"
+
+    model = DistributedModel(
+        args.split_size, 
+        ["worker1", "worker2"],
+        [device, device], 
+        [NNShard1, NNShard2]
+    )
+
+    print(model)
+
+    sys.exit(0)
+    
+    # Define optimizer & loss_fn
+    loss_fn = nn.MSELoss()
+    optimizer = DistributedOptimizer(
+        optim.SGD,
+        model.parameter_rrefs(),
+        lr=args.lr,
+    )
+    # Dataloaders
+
+    (train_dataloader, test_dataloader) = get_torchvision_dataset("MNIST", batch_size)
+
+    
+    print(f"World_size: {world_size}, Rank: {rank}")
+    num_split = 4
+    tik = time.time()
+    run_worker(rank, world_size, model, train_dataloader, test_dataloader, loss_fn, optimizer, epochs, batch_size)
+    tok = time.time()
+    print(f"number of splits = {num_split}, execution time = {tok - tik}")
