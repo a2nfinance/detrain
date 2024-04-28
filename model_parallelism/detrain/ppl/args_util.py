8e81e43935172df76967d3c3036caf0529b52cb5
a2nfinance <levi@a2n.finance>
2024-04-28 22:54:39 +0700

    improve function to init DistributedModel

--- model_parallelism/detrain/ppl/args_util.py
+++ model_parallelism/detrain/ppl/args_util.py
@@ -2,9 +2,9 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description='simple distributed training job based on pipeline parallelism')
     ## General settings
-    parser.add_argument('--epochs', default=10, type=int, help='Total epochs to train the model')
-    parser.add_argument('--batch_size', default=64, type=int, help='Input batch size on each device (default: 32)')
-    parser.add_argument("--split_size", default=4, type=int, help="Number of parts of the input data")
+    parser.add_argument('--epochs', default=3, type=int, help='Total epochs to train the model')
+    parser.add_argument('--batch_size', default=10, type=int, help='Input batch size on each device (default: 32)')
+    parser.add_argument("--split_size", default=2, type=int, help="Number of parts of the input data")
     parser.add_argument('--log', default=None, type=int, 
                         help='Enable training logs, this action can affect to training process performance')
     ## Optimizer settings
@@ -12,10 +12,6 @@ def get_args():
 
     ## Cluster settings
     parser.add_argument('--gpu', default=None, type=int)
-    parser.add_argument('--world-size', default=-1, type=int, 
-                        help='number of nodes for distributed training')
-    parser.add_argument('--rank', default=-1, type=int, 
-                        help='node rank for distributed training')
     # parser.add_argument('--local_rank', default=-1, type=int, 
     #                     help='local rank for distributed training')
 
