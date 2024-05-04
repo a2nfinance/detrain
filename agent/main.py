8a087caf0b927ad7cb56cc89b2d7b866a4de34b1
a2nfinance <levi@a2n.finance>
2024-05-04 21:13:57 +0700

    update host

--- agent/main.py
+++ agent/main.py
@@ -31,4 +31,4 @@ async def do_command(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
