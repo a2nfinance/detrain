5dc206e1d8fa3b20442d3201043bc971280e3f4a
a2nfinance <levi@a2n.finance>
2024-05-04 22:58:20 +0700

    return done

--- agent/command.py
+++ agent/command.py
@@ -6,6 +6,7 @@ def single_execute(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     for c in iter(lambda: process.stdout.read(1), ""):
         yield c
+    yield "done"
 
 def manage_command(command):
     command = command.decode("utf-8")
