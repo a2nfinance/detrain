0a3899b4f0e9165ed9c995c690fb294cfd62932e
a2nfinance <levi@a2n.finance>
2024-05-06 20:53:03 +0700

    update ppl command

--- model_parallelism/setup.py
+++ model_parallelism/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='detrain',
-    version='0.1dev0',
+    version='0.2dev0',
     author='A2N Finance', 
     author_email='levi@a2n.finance',
     packages=find_packages(),
