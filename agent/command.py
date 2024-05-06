1010ea14a98e961cfe88128158a69a6fdf811b3e
a2nfinance <levi@a2n.finance>
2024-05-06 11:48:11 +0700

    add pipsize 2048

--- agent/command.py
+++ agent/command.py
@@ -3,7 +3,7 @@ import os
 
 def single_execute(command):
     command = command.decode("utf-8")
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, pipesize=2048)
     for c in iter(lambda: process.stdout.read(1), b""):
         yield c
 
