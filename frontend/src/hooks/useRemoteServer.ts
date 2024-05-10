c89177a18ef7976601ba19a73d8dad3a7ce1bcd4
a2nfinance <levi@a2n.finance>
2024-05-11 00:33:57 +0700

    receive stream response on the frontend

--- frontend/src/hooks/useRemoteServer.ts
+++ frontend/src/hooks/useRemoteServer.ts
@@ -24,63 +24,67 @@ export async function* streamingFetch(input: RequestInfo | URL, init?: RequestIn
 }
 export const useRemoteServer = () => {
     const dispatch = useAppDispatch();
-    const {parallelForm, trainingScriptForm} = useAppSelector(state => state.setupForms)
+    const { parallelForm, trainingScriptForm } = useAppSelector(state => state.setupForms)
 
     const sendCommand = async (remoteHostIP: string, command: string, outputElementId: string, rank: number, agentPort: number) => {
-
-        let url = `/api/stream`;
-
-        let options = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ remoteHostIP: remoteHostIP, command: command, agentPort:  agentPort}),
-            keepalive: true,
-        }
-        const it = streamingFetch(url, options)
-        let element = document.getElementById(outputElementId);
-        for await (let value of it) {
-            try {
-                element?.append(value)
-            } catch (e: any) {
-                console.warn(e.message)
+        try {
+            let url = `//${remoteHostIP}:${agentPort}/execute/`;
+            if (agentPort === 80) {
+                url = `//${remoteHostIP}/execute/`;
             }
+            let options = {
+                method: 'POST',
+                body: command,
+                keepalive: true,
+            }
+            const it = streamingFetch(url, options)
+            let element = document.getElementById(outputElementId);
+            for await (let value of it) {
+                try {
+                    element?.append(value)
+                } catch (e: any) {
+                    console.warn(e.message)
+                }
+            }
+            if (rank === 0) {
+                dispatch(updateActionStatus({ actionName: actionNames.startTrainingAction, value: false }))
+                dispatch(setFormsProps({ att: "downloadButtonEnable", value: true }))
+            }
+        } catch (e) {
+            console.log(e);
         }
-        if (rank === 0) {
-            dispatch(updateActionStatus({ actionName: actionNames.startTrainingAction, value: false }))
-            dispatch(setFormsProps({ att: "downloadButtonEnable", value: true }))
-        }
-        
+
+
     }
 
     const downloadFile = async (remoteHostIP: string, filePath: string, masterNodeAgentPort: number) => {
-       
-        let url = `/api/file`;
-
-        let options = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ remoteHostIP: remoteHostIP, filePath: filePath, agentPort:  masterNodeAgentPort }),
+        try {
+            let url = `//${remoteHostIP}:${masterNodeAgentPort}/download/`;
+            if (masterNodeAgentPort === 80) {
+                url = `//${remoteHostIP}/download/`;
+            }
+            let options = {
+                method: 'POST',
+                body: filePath
+            }
+    
+            let response = await fetch(url, options)
+            let blob = await response.blob();
+            let href = window.URL.createObjectURL(blob);
+            const a = Object.assign(document.createElement("a"), {
+                href,
+                style: "display:none",
+                download: `${parallelForm.modelName}.pt`,
+            });
+            document.body.appendChild(a);
+            a.click();
+            URL.revokeObjectURL(href);
+            a.remove();
+    
+        } catch(e) {
+            console.log(e)
         }
-
-        const req = await fetch(url, options);
-
-        const blob = await req.blob();
-        console.log(blob)
-        let href = window.URL.createObjectURL(blob);
-        const a = Object.assign(document.createElement("a"), {
-            href,
-            style: "display:none",
-            download: `${parallelForm.modelName}.pt`,
-        });
-        document.body.appendChild(a);
-        a.click();
-        URL.revokeObjectURL(href);
-        a.remove();
-
+        
     }
 
     return { sendCommand, downloadFile };
