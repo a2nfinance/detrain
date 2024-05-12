dc968cba8c28e6e7e1a66606e55cbbdbb487cd98
a2nfinance <levi@a2n.finance>
2024-05-12 16:46:36 +0700

    add comments to code

--- model_parallelism/examples/ppl/nn/main.py
+++ model_parallelism/examples/ppl/nn/main.py
@@ -6,30 +6,36 @@ from detrain.ppl.args_util import get_args
 from detrain.ppl.worker import run_worker
 from detrain.ppl.dataset_util import get_torchvision_dataset
 from shards_model import NNShard1, NNShard2
-from torch.distributed.optim.optimizer import DistributedOptimizer
 import torch.optim as optim
-# Define model here
-# torchrun \
-# --nproc_per_node=1 --nnodes=3 --node_rank=0 \
-# --master_addr=localhost --master_port=9999 \
-# main.py --use_syn
+
 if __name__=="__main__":
+    # Get torchrun args
     args = get_args()
-    # Get args
     world_size = int(os.environ["WORLD_SIZE"])
     rank = int(os.environ["RANK"])
     epochs = int(args.epochs)
     batch_size = int(args.batch_size)
     lr = float(args.lr)
-    device = "cpu"
 
-    # Check devices
-    if (args.gpu is not None):
-        device = "cuda"
-
+    # Check cuda device
     for i in range(torch.cuda.device_count()):
         print(torch.cuda.get_device_properties(i).name)
+
+    # Define params to create a new instance of DistributedModel
+    devices = []
+    workers = []
+    shards = [NNShard1, NNShard2]
     
+    # Devices for model shards
+    if (args.gpu is not None):
+        arr = args.gpu.split('_')
+        for dv in range(len(arr)):
+            if dv > 0:
+                workers.append(f"worker{dv}")
+                if int(arr[dv]) == 1:
+                    devices.append("cuda:0")
+                else:
+                    devices.append("cpu")
 
     # Define optimizer & loss_fn
     loss_fn = nn.CrossEntropyLoss()
@@ -41,16 +47,20 @@ if __name__=="__main__":
 
     
     print(f"World_size: {world_size}, Rank: {rank}")
+    
+    # batch to mini batches
     num_split = 4
     tik = time.time()
+
+    # Start a master node & worker nodes
     run_worker(
         rank, 
         world_size, 
         (
             args.split_size, 
-            ["worker1", "worker2"],
-            [device, device], 
-            [NNShard1, NNShard2]
+            workers,
+            devices, 
+            shards
         ), 
         train_dataloader, 
         test_dataloader, 
@@ -60,5 +70,6 @@ if __name__=="__main__":
         batch_size,
         lr
     )
+
     tok = time.time()
     print(f"number of splits = {num_split}, execution time = {tok - tik}")
