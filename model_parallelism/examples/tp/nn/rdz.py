b84a8eeed94d10fda4316cb6e2057d994bafd4df
a2nfinance <levi@a2n.finance>
2024-05-13 16:47:20 +0700

    correct commands and docs

--- model_parallelism/examples/tp/nn/rdz.py
+++ model_parallelism/examples/tp/nn/rdz.py
@@ -1,7 +1,7 @@
 import torch.distributed as dist
 from torch.distributed.elastic.rendezvous import c10d_rendezvous_backend
 from torch.distributed.elastic.rendezvous import dynamic_rendezvous
-store = dist.TCPStore("localhost", 1522)
+store = dist.TCPStore("localhost", 9999)
 
 backend = c10d_rendezvous_backend.C10dRendezvousBackend(store, "101")
 
